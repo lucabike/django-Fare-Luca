2e2a120d2d79b30cb960b1f92dbbc47bf860c12e
lucabike <lucafare502@gmail.com>
2021-12-09 12:14:22 +0100

    livello 12

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -9,4 +9,6 @@ urlpatterns=[
     path('chi_siamo',chi_siamo,name='chi_siamo'),
     path('variabili',variabili,name='variabili'),
     path('',index,name='index'),
+    
+
 ]
--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'prima_app',
+    'seconda_app',
+    'news',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'primo_progetto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'prima_app/templates'),os.path.join(BASE_DIR, 'seconda_app/templates'),os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'prima_app/templates'),os.path.join(BASE_DIR, 'seconda_app/templates'),os.path.join(BASE_DIR, 'news/templates'),os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
