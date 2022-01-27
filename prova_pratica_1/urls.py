dd7c5da45091b59ecccab2131ee6c30d6666358d
lucabike <lucafare502@gmail.com>
2022-01-27 11:27:27 +0100

    view_D

--- prova_pratica_1/urls.py
+++ prova_pratica_1/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from .views import view_b
 from .views import view_c
+from .views import view_d
 
 
 app_name="prova_pratica_1"
 urlpatterns=[
     path('view_b',view_b,name='view_b'),
     path('view_c',view_c,name='view_c'),
+    path('view_d',view_d,name='view_d'),
+
 ]
 
 
